0c9a680f05c0e700300cf955536c6dc5724c7847
David <armillas7@gmail.com>
2020-02-21 21:40:01 +0100

    Afegida capa de països i qualitat democràtica

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var year = 1946;
+    var year = 1948;
     var map;
 
     addMap();
@@ -19,22 +19,41 @@ $(document).ready(function () {
             function addCountries() {
                 map.addSource("countries-source", {
                     type: "vector",
-                    url: "mapbox://armillas7.a8m6dwuc"
+                    url: "mapbox://armillas7.clyn6acm"
                 });
 
                 map.addLayer({
                     id: "countries",
                     type: "fill",
                     source: "countries-source",
-                    'source-layer': "shp-7izibe",
+                    'source-layer': "democracy-countries-8h1h1r",
                     'paint': {
                         'fill-color': {
-                            property: 'prova_' + year,
+                            property: 'qd_' + year,
                             type: 'interval',
                             stops: [
-                                [6, '#e7d01d'],
-                                [8, '#be801e'],
-                                [10, '#e7271d']
+                                [-15, '#FAFAFA'],
+                                [-10, '#FF1010'],
+                                [-9, '#FE1D0E'],
+                                [-8, '#FD2B0C'],
+                                [-7, '#FC390A'],
+                                [-6, '#FB4708'],
+                                [-5, '#FA5507'],
+                                [-4, '#F96305'],
+                                [-3, '#F87103'],
+                                [-2, '#F77F01'],
+                                [-1, '#F78D00'],
+                                [0, '#E08603'],
+                                [1, '#CA8007'],
+                                [2, '#B47A0A'],
+                                [3, '#9D740E'],
+                                [4, '#876E11'],
+                                [5, '#716715'],
+                                [6, '#5B6118'],
+                                [7, '#445B1C'],
+                                [8, '#2E551F'],
+                                [9, '#184F23'],
+                                [10, '#024927']
                             ]
                         },
                         'fill-opacity': 0.4
@@ -46,20 +65,39 @@ $(document).ready(function () {
 
     function addSlider() {
         $("#slider").slider({
-            orientation: "vertical",
-            value: 1946,
-            min: 1946,
+            value: 1948,
+            min: 1948,
             max: 2018,
             step: 1,
             slide: function (event, ui) {
                 year = ui.value;
+                $(".info-year-txt").text(year);
                 map.setPaintProperty('countries', 'fill-color', {
-                    property: 'prova_' + year,
+                    property: 'qd_' + year,
                     type: 'interval',
                     stops: [
-                        [6, '#e7d01d'],
-                        [8, '#be801e'],
-                        [10, '#e7271d']
+                        [-15, '#FAFAFA'],
+                        [-10, '#FF1010'],
+                        [-9, '#FE1D0E'],
+                        [-8, '#FD2B0C'],
+                        [-7, '#FC390A'],
+                        [-6, '#FB4708'],
+                        [-5, '#FA5507'],
+                        [-4, '#F96305'],
+                        [-3, '#F87103'],
+                        [-2, '#F77F01'],
+                        [-1, '#F78D00'],
+                        [0, '#E08603'],
+                        [1, '#CA8007'],
+                        [2, '#B47A0A'],
+                        [3, '#9D740E'],
+                        [4, '#876E11'],
+                        [5, '#716715'],
+                        [6, '#5B6118'],
+                        [7, '#445B1C'],
+                        [8, '#2E551F'],
+                        [9, '#184F23'],
+                        [10, '#024927']
                     ]
                 });
             }
